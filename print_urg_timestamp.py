11a6688463435b38f951819638436b0732d58106
Gao Shuqi <30630975+Tenant@users.noreply.github.com>
2021-03-05 20:24:08 +0800

    Add files via upload

---
+++ print_urg_timestamp.py
@@ -0,0 +1,29 @@
+import sys, struct
+
+def print_urg_timestamp(fn):
+    # timestampe: long, uint32
+    # data: uint16 * bim numbers
+    with open(fn, "rb") as f:
+        p_data = f.read(4)
+        if len(p_data) < 4:
+            print("Cannot open file")
+            return
+        ang_range = struct.unpack('<f', p_data)[0] # ang range: float, 4 byte, = 180 degreee
+        p_data = f.read(4)
+        ang_resol = struct.unpack('<f', p_data)[0] # ang resolution: float, 4 byte, = 0.125 degree
+        p_data = f.read(4)
+        unit = struct.unpack('<f', p_data)[0] # unit: float, 4 byte, = 100
+        print("angle range: {}, angle resolution: {}, unit: {}".format(ang_range, ang_resol, unit))
+        bim_numbers = int(ang_range // ang_resol + 1)
+        print("bim number: {}".format(bim_numbers))
+        while True:
+            p_data = f.read(4)
+            if len(p_data) < 4:
+                break
+            timestamp = struct.unpack('<l', p_data)[0]
+            p_data = f.read(bim_numbers * 2)
+            print(timestamp)
+            
+
+if __name__ == '__main__':
+    print_urg_timestamp(sys.argv[1])
